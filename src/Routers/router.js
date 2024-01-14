b368086d4bfd9a1729c0fcdc7ae27408cabce626
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-01-14 11:29:58 -0300

    feat: add patient registration route

--- src/Routers/router.js
+++ src/Routers/router.js
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Layout from "../components/Layout";
 import Services from "../page/Planos";
 import Patients from "../page/Patients";
+import HospitalRegistrationForm from "../page/Register";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<HospitalRegistrationForm />} />
           <Route path="plans" element={<Services />} />
           <Route path="pacientes" element={<Patients />} />
         </Route>
