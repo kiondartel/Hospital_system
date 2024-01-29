214040b4d13665e4af2e77001fc2c92c81ae4783
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-01-29 08:37:17 -0300

    add new side bar with antd submenu and improvement in styles sidebar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,16 +1,93 @@
-import { SidebarContainer, SidebarNav, SidebarLink, UserPhoto } from "./styles";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaUserMd, FaClipboardList, FaDollarSign } from "react-icons/fa";
+import { UserPhoto, SidebarContainer, StyledMenu } from "./styles";
+
+const MenuItemLink = ({ to, children }) => <Link to={to}>{children}</Link>;
 
 const Sidebar = () => {
+  const [openKeys, setOpenKeys] = useState([]);
+
+  const onOpenChange = (keys) => {
+    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+    setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+  };
+
+  const menuItems = [
+    {
+      label: (
+        <span>
+          <FaUserMd />
+          <span>Pacientes</span>
+        </span>
+      ),
+      key: "sub1",
+      children: [
+        {
+          label: <MenuItemLink to="/">Novo Paciente</MenuItemLink>,
+          key: "1",
+        },
+        {
+          label: (
+            <MenuItemLink to="/pacientes/lista">
+              Lista de Pacientes
+            </MenuItemLink>
+          ),
+          key: "2",
+        },
+      ],
+    },
+    {
+      label: (
+        <span>
+          <FaClipboardList />
+          <span>Planos</span>
+        </span>
+      ),
+      key: "sub2",
+      children: [
+        {
+          label: <MenuItemLink to="/plans">Gestão de planos</MenuItemLink>,
+          key: "3",
+        },
+      ],
+    },
+    {
+      label: (
+        <span>
+          <FaDollarSign />
+          <span>Financeiro</span>
+        </span>
+      ),
+      key: "sub3",
+      children: [
+        {
+          label: (
+            <MenuItemLink to="/financeiro/clients">Colaboradores</MenuItemLink>
+          ),
+          key: "4",
+        },
+        {
+          label: (
+            <MenuItemLink to="/financeiro">Fechamento do mes</MenuItemLink>
+          ),
+          key: "5",
+        },
+      ],
+    },
+  ];
+
   return (
     <SidebarContainer>
-      <SidebarNav>
-        <UserPhoto />
-        <p>Gabriel Lucas</p>
-        <SidebarLink to="/pacientes">Pacientes</SidebarLink>
-        <SidebarLink to="/plans">Planos</SidebarLink>
-        <SidebarLink to="/agendamentos">Agendamentos</SidebarLink>
-        <SidebarLink to="/financeiro">Financeiro</SidebarLink>
-      </SidebarNav>
+      <UserPhoto />
+      <p>Gabriel Lucas</p>
+      <StyledMenu
+        theme="dark"
+        mode="inline"
+        openKeys={openKeys}
+        onOpenChange={onOpenChange}
+        items={menuItems}
+      />
     </SidebarContainer>
   );
 };
--- src/components/SideBar/styles.js
+++ src/components/SideBar/styles.js
@@ -1,36 +1,36 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import user from "../../assets/image/gabriel.jpg";
+import { Menu } from "antd";
 
 export const SidebarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 250px;
   height: 100vh;
   background-color: #f0f0f0;
   padding: 20px;
   box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
 `;
 
 export const UserPhoto = styled.img`
   content: url(${user});
   max-width: 70px;
   border-radius: 50%;
+  margin-bottom: 20px;
 `;
 
-export const SidebarNav = styled.nav`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const SidebarLink = styled(Link)`
-  padding: 10px 15px;
-  text-decoration: none;
-  color: #333;
-  font-weight: bold;
-  transition: color 0.3s;
-
-  &:hover {
-    color: #007bff;
+export const StyledMenu = styled(Menu)`
+  background-color: #fff;
+  margin-top: 10px;
+  width: 250px;
+  .ant-menu-submenu-open,
+  .ant-menu-item-selected {
+    border-left: 4px solid purple;
+    background-color: #fff;
+  }
+  .ant-menu-item {
+    border-left: 4px solid transparent !important;
   }
 `;
