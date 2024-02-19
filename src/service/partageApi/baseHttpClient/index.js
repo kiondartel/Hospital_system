7857c721945813081f04c7c5f9dd1ae051ee9de9
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-02-19 09:39:17 -0300

    Feat: Add BaseHttpClient class for centralized HTTP requests handling

---
+++ src/service/partageApi/baseHttpClient/index.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+class BaseHttpClient {
+  constructor(headers) {
+    const baseURL = process.env.REACT_APP_API_BASE_URL || "";
+
+    this.instance = axios.create({
+      baseURL,
+      headers,
+    });
+  }
+
+  get(url, params, config) {
+    return this.instance.get(url, {
+      params,
+      ...config,
+    });
+  }
+
+  post(url, data) {
+    return this.instance.post(url, data);
+  }
+
+  put(url, data) {
+    return this.instance.put(url, data);
+  }
+}
+
+export default BaseHttpClient;
