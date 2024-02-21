bbb944b3bbb7fdf1c171d84c76b355ea0c59b79c
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-02-21 18:51:59 -0300

    feat: create listUsers reducer

---
+++ src/store/Reducers/useListRuducer.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  data: [],
+};
+
+export const listUsersSlice = createSlice({
+  name: "listUsers",
+  initialState,
+  reducers: {
+    setListUsers: (state, { payload }) => {
+      state.initialState = payload;
+    },
+  },
+});
+
+export const { setListUsers } = listUsersSlice.actions;
+export default listUsersSlice.reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import sliceMedicalPlans from "./Reducers/medicalPlansReducer";
 
 import planSlice from "./Reducers/selectPlanReducer";
+import listUsersSlice from "./Reducers/useListRuducer";
 const store = configureStore({
   reducer: {
     medicalPlans: sliceMedicalPlans,
     plan: planSlice,
+    users: listUsersSlice,
   },
 });
 
