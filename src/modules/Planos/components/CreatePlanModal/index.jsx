9bc8b1c22a7dde42a8d3fc2b1d02a357fb7ea2f4
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-02-26 14:59:20 -0300

    feat: new modal create by plans

---
+++ src/modules/Planos/components/CreatePlanModal/index.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Modal, Form, Input, InputNumber, Button } from "antd";
+
+const CreatePlanModal = ({ isOpen, onOk, onCancel }) => {
+  const [form] = Form.useForm();
+
+  const handleSubmit = () => {
+    form
+      .validateFields()
+      .then((values) => {
+        form.resetFields();
+        onOk(values);
+      })
+      .catch((info) => {
+        console.log("Validate Failed:", info);
+      });
+  };
+
+  return (
+    <Modal
+      title="Adicionar Novo Plano"
+      open={isOpen}
+      onOk={handleSubmit}
+      onCancel={onCancel}
+      footer={[
+        <Button key="back" onClick={onCancel}>
+          Cancelar
+        </Button>,
+        <Button key="submit" type="primary" onClick={handleSubmit}>
+          Criar Plano
+        </Button>,
+      ]}
+    >
+      <Form form={form} layout="vertical">
+        <Form.Item
+          name="nome"
+          label="Nome do Plano"
+          rules={[
+            { required: true, message: "Por favor, insira o nome do plano!" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="valor"
+          label="Valor"
+          rules={[
+            { required: true, message: "Por favor, insira o valor do plano!" },
+          ]}
+        >
+          <InputNumber style={{ width: "100%" }} />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default CreatePlanModal;
