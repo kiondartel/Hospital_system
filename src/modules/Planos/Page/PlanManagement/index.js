ba0e7122cd0916b0ab7cfab1107ae6d18eedcbd8
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-02-26 14:58:24 -0300

    refactor: Enhance PlansManagement layout with additional columns and styles

--- src/modules/Planos/Page/PlanManagement/index.js
+++ src/modules/Planos/Page/PlanManagement/index.js
@@ -1,27 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchMedicalPlans } from "../../../../store/Actions/medicalPlansActions";
+import { List, Button } from "antd";
+import {
+  PlanActions,
+  PlanName,
+  PlanValue,
+  StyledCard,
+  StyledListItem,
+} from "./styles";
+
+import CreatePlanModal from "../../components/CreatePlanModal/index";
 
 const PlansManagement = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const medicalPlans = useSelector((state) => state.medicalPlans);
   const allPlans = medicalPlans.data;
   const dispatch = useDispatch();
+
+  const handleCreatePlan = (planData) => {
+    console.log("New Plan Data:", planData);
+    setIsModalVisible(false);
+  };
 
   useEffect(() => {
     dispatch(fetchMedicalPlans());
   }, []);
 
   return (
-    <div>
-      <h1>Planos</h1>
-      <ul>
-        {allPlans?.map((plano) => (
-          <li key={plano.codigo}>
-            {plano.nome} - R$ {plano.valor}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <StyledCard title="Planos">
+      <Button
+        type="primary"
+        onClick={() => setIsModalVisible(true)}
+        style={{ marginBottom: 16 }}
+      >
+        Adicionar Plano
+      </Button>
+      <CreatePlanModal
+        isOpen={isModalVisible}
+        onOk={handleCreatePlan}
+        onCancel={() => setIsModalVisible(false)}
+      />
+      <List
+        dataSource={allPlans}
+        renderItem={(plano) => (
+          <StyledListItem key={plano.codigo}>
+            <div>
+              <PlanName>{plano.nome}</PlanName>
+              <PlanValue>- R$ {plano.valor}</PlanValue>
+            </div>
+            <PlanActions>
+              <Button type="primary">Editar</Button>
+              <Button type="danger">Deletar</Button>
+            </PlanActions>
+          </StyledListItem>
+        )}
+      />
+    </StyledCard>
   );
 };
 
---
+++ src/modules/Planos/Page/PlanManagement/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { Card, List } from "antd";
+
+export const StyledCard = styled(Card)`
+  margin: 20px;
+  border-radius: 10px;
+`;
+
+export const StyledListItem = styled(List.Item)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const PlanName = styled.span`
+  font-size: 16px;
+  font-weight: bold;
+  color: #003366;
+`;
+
+export const PlanValue = styled.span`
+  font-size: 14px;
+  color: #008000;
+  margin-left: 10px;
+`;
+
+export const PlanActions = styled.div`
+  display: flex;
+  gap: 8px;
+`;
