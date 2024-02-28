008c63d3ba8068cb171e8b54914eb2bbe74c4808
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-02-28 09:06:11 -0300

    feat: implement createNewPlan action

--- src/store/Actions/medicalPlansActions.js
+++ src/store/Actions/medicalPlansActions.js
@@ -1,5 +1,6 @@
 import { PlansService } from "../../service/partageApi/MedicalPlansService";
 import { setMedicalPlans } from "../Reducers/medicalPlansReducer";
+import { notification } from "antd";
 
 export const fetchMedicalPlans = () => async (dispatch) => {
   try {
@@ -7,5 +8,22 @@ export const fetchMedicalPlans = () => async (dispatch) => {
     dispatch(setMedicalPlans(response.data));
   } catch (err) {
     console.error(err);
+    return notification.error({
+      message: "Erro ao buscar planos",
+    });
+  }
+};
+
+export const createNewPlan = async (payload) => {
+  try {
+    await PlansService.createPlan(payload);
+    return notification.success({
+      message: "Plano criado com sucesso!",
+    });
+  } catch (err) {
+    console.log(err);
+    return notification.error({
+      message: "Erro ao criar plano",
+    });
   }
 };
