dc9372141bb6be5c9993fa2c5533d9bc3d44fb5d
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-02-28 08:58:47 -0300

    feat: add createPlan method to PlansService for plan creation

--- src/service/partageApi/MedicalPlansService.js
+++ src/service/partageApi/MedicalPlansService.js
@@ -6,4 +6,7 @@ export class PlansService {
   static async getAllPlans() {
     return partageApi.get(PLANS_PATH);
   }
+  static async createPlan(payload) {
+    return partageApi.post(PLANS_PATH, payload);
+  }
 }
