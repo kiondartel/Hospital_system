3de78725e037c65bf9819a9341ef2a4da0164c9e
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-02-29 16:06:40 -0300

    Feat: add new drawer modal for HospitalRegistrationForm

---
+++ src/modules/Register/components/Planos/HospitalRegistrationModal/index.jsx
@@ -0,0 +1,18 @@
+import { Drawer } from "antd";
+import HospitalRegistrationForm from "../../../Pages/PatientList/Components/NewPatient";
+
+const HospitalRegistrationModal = ({ open, onClose }) => {
+  return (
+    <Drawer
+      title="Cadastro de Pacientes"
+      placement="right"
+      onClose={onClose}
+      open={open}
+      width={720}
+    >
+      <HospitalRegistrationForm />
+    </Drawer>
+  );
+};
+
+export default HospitalRegistrationModal;
