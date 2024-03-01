fe382e88ab00beef7d8419f024ce4c521ee3b994
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-03-01 07:58:36 -0300

    Feat: Update routing to set Dashboard as Home Page

--- src/Routers/router.js
+++ src/Routers/router.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "../components/Layout";
 import PlansManagement from "../modules/Planos/Page/PlanManagement/index";
 import PatientList from "../modules/Register/Pages/PatientList";
+import HospitalDashboard from "../modules/DashBoard/Index";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<HospitalDashboard />} />
           <Route path="patient/list" element={<PatientList />} />
           <Route path="plans" element={<PlansManagement />} />
         </Route>
