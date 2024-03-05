230a79ca46a6eb1a17e206835d203a6ea9dec0ad
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-03-05 11:39:44 -0300

    Feat: add button to view user details modal

--- src/modules/DashBoard/Components/LatestUsers/index.jsx
+++ src/modules/DashBoard/Components/LatestUsers/index.jsx
@@ -1,19 +1,33 @@
-import React, { useEffect } from "react";
-import { Table, Typography } from "antd";
+import React, { useEffect, useState } from "react";
+import { Button, Table, Typography } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllUsers } from "../../../../store/Actions/UsersActions";
 import { Container } from "./styles.js";
+import UserDetailsModal from "../../../../components/UserDetailsModal/index.jsx";
+import formatDate from "../../../Utils/dateUtils.js";
 
 const LatestUsers = () => {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.users);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const sortedUsers = [...users.data].sort(
+    (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+  );
+
+  const showUserDetails = (user) => {
+    setSelectedUser(user);
+    setIsModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalVisible(false);
+  };
+
   useEffect(() => {
     dispatch(getAllUsers());
   }, []);
 
-  const sortedUsers = [...users.data].sort(
-    (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
-  );
   const columns = [
     {
       title: "Nome",
@@ -36,6 +50,21 @@ const LatestUsers = () => {
       key: "plano.nome",
       render: (plano) => plano?.nome,
     },
+    {
+      title: "Data da entrada",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (createdAt) => formatDate(createdAt),
+    },
+    {
+      title: "Prontuario",
+      key: "nome",
+      render: (text, record) => (
+        <Button type="primary" onClick={() => showUserDetails(record)}>
+          Ver Prontuário
+        </Button>
+      ),
+    },
   ];
 
   return (
@@ -47,6 +76,11 @@ const LatestUsers = () => {
         dataSource={sortedUsers}
         columns={columns}
         pagination={{ pageSize: 5 }}
+      />
+      <UserDetailsModal
+        user={selectedUser}
+        isVisible={isModalVisible}
+        handleClose={handleModalClose}
       />
     </Container>
   );
