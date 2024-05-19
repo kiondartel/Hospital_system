00129bba143aa640d1530241009061e4d03a23a8
Gabriel Lucas <gabriellucasseverino@hotmail.com>
2024-05-18 22:41:03 -0300

    feat(routes): implement granular suspense for lazy-loaded components

--- src/Routers/router.js
+++ src/Routers/router.js
@@ -1,18 +1,43 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import Layout from "../components/Layout";
-import PlansManagement from "../modules/Planos/Page/PlanManagement/index";
-import PatientList from "../modules/Register/Pages/PatientList";
-import HospitalDashboard from "../modules/DashBoard/Index";
+// Importação lazy dos componentes
+const Layout = lazy(() => import("../components/Layout"));
+const PlansManagement = lazy(() =>
+  import("../modules/Planos/Page/PlanManagement/index")
+);
+const PatientList = lazy(() => import("../modules/Register/Pages/PatientList"));
+const HospitalDashboard = lazy(() => import("../modules/DashBoard/Index"));
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<HospitalDashboard />} />
-          <Route path="patient/list" element={<PatientList />} />
-          <Route path="plans" element={<PlansManagement />} />
+          <Route
+            index
+            element={
+              <Suspense fallback={<div>Carregando...</div>}>
+                <HospitalDashboard />
+              </Suspense>
+            }
+          />
+          <Route
+            path="patient/list"
+            element={
+              <Suspense fallback={<div>Carregando...</div>}>
+                <PatientList />
+              </Suspense>
+            }
+          />
+          <Route
+            path="plans"
+            element={
+              <Suspense fallback={<div>Carregando...</div>}>
+                <PlansManagement />
+              </Suspense>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
